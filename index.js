afb7b26de27e570a87b345e87594dca2f4097ed3
Esther Kang <eskang87@gmail.com>
2021-11-26 10:43:36 -0800

    commit tree cfb42b5466d4b6fb82788aaf19ac22e367e0844e

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (arrayParam) => {
+    return arrayParam.slice(0, 2);
+};
+
+const returnLastTwoDrivers = (arrayParam) => {
+    return arrayParam.slice(-2);
+};
+
+/* function selectingDrivers(){
+    let newArray;
+    newArray[0] = returnFirstTwoDrivers();
+    newArray[1] = returnLastTwoDrivers();
+    return newArray;
+}; */
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(numVal){
+    const multFare = () => numVal * numVal;
+    return multFare; 
+};
+
+const fareMultiplier = function(param) {
+    const multFare =  param * 2;
+    return multFare;
+};
+
+const fareQuintupler = function(param) {
+    const quintMult = param * 5;
+    return quintMult;
+};
+
+function fareDoubler(numVal) {
+    const doubleFare = numVal * 2;
+    return doubleFare;
+};
+
+function fareTripler(numVal) {
+    const tripleFare = numVal * 3;
+    return tripleFare;
+};
+
+function selectDifferentDrivers(array, func) {
+    let returnArray = func(array);
+    return returnArray;
+};
